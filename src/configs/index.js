fe9f3a55eabafe0156a7bfe068b0944f8c064e49
kanya k <>
2018-04-16 21:37:29 +0700

    add RNMGL (not test yet)

---
+++ src/configs/index.js
@@ -0,0 +1,7 @@
+import env from '../../env.json'
+
+const MAPBOX_TOKEN = env.mapboxToken
+
+export {
+  MAPBOX_TOKEN,
+}
--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,18 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   View,
+  Platform,
   StyleSheet,
 } from 'react-native'
+import MapboxGL from '@mapbox/react-native-mapbox-gl'
+import { MAPBOX_TOKEN } from '../configs'
 
-const Map = () => (
-  <View
-    testID="map"
-    style={styles.container}
-  />
-)
+MapboxGL.setAccessToken(MAPBOX_TOKEN)
+
+class Map extends Component {
+  state = {
+    isGranted: false,
+  }
+
+  componentDidMount() {
+    if (Platform.OS === 'android') {
+      this.getAndroidPermission()
+    } else {
+      this.setState({ isGranted: true })
+    }
+  }
+
+  getAndroidPermission = async () => {
+    try {
+      const isGranted = await MapboxGL.requestAndroidLocationPermissions()
+      console.warn('isgrant = ', isGranted)
+      this.setState({ isGranted })
+    } catch(error) {
+      console.warn('err ', error)
+      this.setState({ isGranted: false })
+    }
+  }
+
+  render() {
+    return(
+      <MapboxGL.MapView
+        testID="map"
+        style={styles.map}
+        centerCoordinate={[100, 13]}
+        showUserLocation={this.state.isGranted}
+      />
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  map: {
     flex: 1,
   }
 })
